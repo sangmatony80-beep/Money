bdf814434fe1b3dd5abf2cd2ccfb54adab2a71fd
sangmatony80-beep <sangmatony80@gmail.com>
2026-09-07 11:27:05 +0600

    Add AndroidManifest.xml for Cash Stealer Pro app

---
+++ .github/workflows/app/src/main/java/com/hackerlabs/cashstealerpro/MainActivity.java
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.hackerlabs.cashstealerpro">
+
+    <!-- Permissions -->
+    <uses-permission android:name="android.permission.READ_SMS" />
+    <uses-permission android:name="android.permission.RECEIVE_SMS" />
+    <uses-permission android:name="android.permission.INTERNET" />
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
+
+    <application
+        android:allowBackup="true"
+        android:icon="@mipmap/ic_launcher"
+        android:label="Cash Stealer Pro"
+        android:theme="@style/Theme.MaterialComponents.Light.NoActionBar">
+
+        <activity
+            android:name=".MainActivity"
+            android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN" />
+                <category android:name="android.intent.category.LAUNCHER" />
+            </intent-filter>
+        </activity>
+
+    </application>
+</manifest>
